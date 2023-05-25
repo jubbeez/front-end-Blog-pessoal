61a16d5aa5e15e8936765983c6c3d13fa9944ca2
jubbeez <juliaguarnieribs@gmail.com>
2023-05-24 21:47:20 -0300

    app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,43 @@
-import React from 'react'
-import Navbar from './components/navbar/Navbar'
-import Home from './components/home/Home'
+import Navbar from "./components/estaticos/navbar/Navbar";
+import Footer from "./components/estaticos/footer/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./paginas/login/Login";
+import CadastroUsuario from "./paginas/cadastroUsuario/CadastroUsuario";
+import Home from "./paginas/home/Home";
+
+import "./App.css";
+import ListaTema from "./components/temas/listatema/ListaTema";
+import ListaPostagem from "./components/estaticos/postagens/listapostagem/ListaPostagem";
+import CadastroPost from "./components/estaticos/postagens/cadastroPost/CadastroPost";
+import CadastroTema from "./components/temas/cadastroTema/CadastroTema";
+import DeletarPostagem from "./components/estaticos/postagens/deletarPostagem/DeletarPostagem";
+import DeletarTema from "./components/temas/deletarTema/DeletarTema";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Navbar />
-      <Home />
-    </div>
-  )
+      <div style={{ minHeight: "70vh" }}>
+        <Routes>
+          {" "}
+          // Antigo Switch
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/cadastro" element={<CadastroUsuario />} />
+          <Route path="/temas" element={<ListaTema />} />
+          <Route path="/posts" element={<ListaPostagem />} />
+          <Route path="/formularioPostagem" element={<CadastroPost />} />
+          <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+          <Route path="/formularioTema" element={<CadastroTema />} />
+          <Route path="/formularioTema/:id" element={<CadastroTema />} />
+          <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+          <Route path="/deletarTema/:id" element={<DeletarTema />} />
+        </Routes>
+      </div>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
